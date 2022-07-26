ed29a78d960e71a652ba52f4816e9d1f867719d0
Farshad.Sheydaei <Farshad.Sheydaei@yahoo.com>
2022-07-26 17:13:35 +0430

    Connected to database.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import mysql.connector
 from datetime import datetime
 
 
@@ -5,3 +6,7 @@ def now():
     now_time = datetime.now()
     date = now_time.strftime("%d/%m/%Y %H:%M:%S")
     return date
+
+conn = mysql.connector.connect(host='localhost', user='root', password='', database='Btc_Price')
+cursor = conn.cursor()
+conn.close()
